e173dcd0739153a0e956ae4347bb2978962fce36
larinhaz <158524041+larinhaz@users.noreply.github.com>
2024-04-11 21:49:47 -0300

    Update, Python.
    Fiz a atividade do dia 11/04.

---
+++ aula 1 - lógica de programação/aula02.py
@@ -0,0 +1,13 @@
+#print("olá mundo, finalmente quebrei a maldição")
+
+
+#comentario
+
+print("Olá, bem vindo ao nosso app!")
+
+nome = input("Qual o seu nome?:") 
+idade = input("Qual a sua idade?:")
+gosto = input("Qual é o seu hobbie?:")
+
+print(f"Olá {nome}, você tem {idade} anos e você gosta de:{gosto}")
+
